8c994ad98414bc2d33efc4b45e0024add819dde6
realview45 <kjink0125@gmail.com>
2023-02-17 22:22:08 +0900

    1초간격으로 노트 떨어뜨리기

---
+++ src/dynamic_beat_14/KeyListener.java
@@ -0,0 +1,81 @@
+package dynamic_beat_14;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter{
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(DynamicBeat.game==null) {//현재 게임이 진행되고있지 않다면 끝내기 키보드눌러도 변화X
+			return;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			DynamicBeat.game.pressA();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_S) {
+			DynamicBeat.game.pressS();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_D) {
+			DynamicBeat.game.pressD();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_F) {
+			DynamicBeat.game.pressF();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			DynamicBeat.game.pressSB();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_J) {
+			DynamicBeat.game.pressJ();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_K) { 
+			DynamicBeat.game.pressK();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_L) {
+			DynamicBeat.game.pressL();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SEMICOLON) {
+			DynamicBeat.game.presssem();
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(DynamicBeat.game==null) {//현재 게임이 진행되고있지 않다면 끝내기 키보드눌러도 변화X
+			return;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			DynamicBeat.game.releaseA();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_S) {
+			DynamicBeat.game.releaseS();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_D) {
+			DynamicBeat.game.releaseD();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_F) {
+			DynamicBeat.game.releaseF();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			DynamicBeat.game.releaseSB();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_J) {
+			DynamicBeat.game.releaseJ();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_K) {
+			DynamicBeat.game.releaseK();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_L) {
+			DynamicBeat.game.releaseL();
+
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SEMICOLON) {
+			DynamicBeat.game.releasesem();
+
+		}
+	}
+}
---
+++ src/dynamic_beat_14/Note.java
@@ -0,0 +1,77 @@
+//13
+package dynamic_beat_14;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Note extends Thread{
+	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/Note01.png")).getImage();
+	//노트가 생성되고 1초뒤에 판정라인에 다다르게됨
+	//현재위치 120 1초에 700이동 1초가 지났을때 580이라는 판정라인에다다름
+	private int x,y=580-1000/Main.SLEEP_TIME*Main.NOTE_SPEED;
+	private String noteType;
+	
+	//14
+	//public Note(int x, int y, String noteType) {
+	public Note(String noteType) {
+		if(noteType.equals("A")) {
+			x=123;
+		}
+		else if(noteType.equals("S")) {
+			x=227;
+		}
+		else if(noteType.equals("D")) {
+			x=331;
+		}
+		else if(noteType.equals("F")) {
+			x=435;
+		}
+		else if(noteType.equals("SB")) {
+			x=539;
+		}
+		else if(noteType.equals("J")) {
+			x=643;
+		}
+		else if(noteType.equals("K")) {
+			x=747;
+		}
+		else if(noteType.equals("L")) {
+			x=851;
+		}
+		else if(noteType.equals(";")) {
+			x=955;
+		}
+		this.noteType=noteType;
+	}
+	
+	public void screenDraw(Graphics2D g) {
+		//14
+		if(!noteType.equals("SB"))
+		{
+			g.drawImage(noteBasicImage, x, y, null);
+		}
+		else //스페이스바
+		{
+			g.drawImage(noteBasicImage, x, y, null);
+			g.drawImage(noteBasicImage, x+100, y, null);
+
+		}
+	}
+	public void drop() {
+		y+=Main.NOTE_SPEED;
+	}
+	@Override 
+	public void run() {
+		try {
+			while(true) {//무한반복
+				drop();
+				//떨어지고 SLEEPTIME만큼 0.01초쉴수있도록함= 1초에 100번실행700만큼
+				Thread.sleep(Main.SLEEP_TIME);
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
